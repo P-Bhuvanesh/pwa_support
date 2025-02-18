5abfbb5bcd723531390492498f186e618ff0d226
P-Bhuvanesh <pbhuvanesh345@gamil.com>
2025-02-18 13:33:31 +0530

    ports changed

--- app.py
+++ app.py
@@ -11,5 +11,6 @@ def get_data():
     data = request.json.get('text', '')
     return jsonify({"message": f"Received: {data}"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
